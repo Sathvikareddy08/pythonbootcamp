9c34a2d6f5680e88c118aea1d0949a3de2bd84d0
Sathvikareddy08 <narenderreddynayini04@gmail.com>
2024-07-23 20:08:47 +0530

    Add files via upload
    pythonbootcamp

---
+++ arthemetic.py
@@ -0,0 +1,41 @@
+''''a=int(input("enter value a \n"))
+b=int(input("enter value b\n"))
+print(f" the addition of values is {a+b}")
+print(f" the multiplication of values is {a*b}")
+print(f" the power is {a**b}")
+age=int(input("enter age"))
+if(age>=18 and age<24):
+ print("only two wheeler")
+elif(age>=24 and age<=45):
+ print("you are eligible for 2 wheeler")
+else:
+ print("you are eligible for 2,4 and 6 wheeler")
+  apples=int(input())
+  bananas=int(input())
+ oranges=int(input())
+ total=int(input())
+ cost=(apples*15)+(oranges*5)+(bananas*4)
+ if cost<=total:
+   print("good vendor")
+ else:
+   print("bad vendor")'''       
+x=int(input()) 
+if x>0:
+  if x%2==0 and x>0:
+    print("positive even")
+  else:
+    print("positive odd")
+
+ if x%2==0 and x<0:
+ print("negetive even")
+else:
+ if x%2=
+ print("negetive odd")
+else:
+ print("zero is neither even nor odd and neither -ve nor +ve")
+
+             
+
+
+
+             
---
+++ datatypes.py
@@ -0,0 +1,21 @@
+'''print("sridevi college")
+print("im just a girl")
+name="python"
+print("good morning",name)
+var1=input("enter your name\n")
+print("my name is",var1)
+print("my name is (var1)")
+name=input("enter your name")
+age=int(input("enter your age"))
+p''years old") 
+name=input("enter Name")
+math,phy,chem,bio,science= map(int,input().split())
+sum=math+phy+chem+bio+science
+avg=sum/5
+print(f"hello {name} your average marks are {avg}")'''
+name= (input("emter ur name\n"))
+age = int(input("enter your age\n"))
+if age<18:
+ print(f"hello {name} ur age is eligible")
+else:
+ print(f"hello {name} ur not eligible")   
